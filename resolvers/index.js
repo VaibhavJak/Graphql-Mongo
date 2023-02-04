922da3bf5a9917eee321b6f30876377faf87d3d0
vaibhav <jakkulavaibhav@gmail.com>
2023-02-04 10:17:15 +0530

    Initial commit

---
+++ resolvers/index.js
@@ -0,0 +1,10 @@
+import {userQuery,userMutation} from "./user";
+const UserQuery={
+    Query:{
+        ...userQuery
+    },
+    Mutation:{
+        ...userMutation
+    }
+}
+export default UserQuery;
